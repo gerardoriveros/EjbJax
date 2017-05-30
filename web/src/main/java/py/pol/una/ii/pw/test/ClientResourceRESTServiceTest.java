75517204f4942b9924241d0943cbb60eddc1a761
Gerardo <gerardojriveros@gmail.com>
2017-05-30 01:07:59 -0400

    pruebatest2

--- web/src/main/java/py/pol/una/ii/pw/test/ClientResourceRESTServiceTest.java
+++ web/src/main/java/py/pol/una/ii/pw/test/ClientResourceRESTServiceTest.java
@@ -1,20 +1,24 @@
 package py.pol.una.ii.pw.test;
 
 
-import static org.junit.Assert.*;
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
+//import static org.junit.Assert.*;
+//import io.restassured.RestAssured.*;
+//import io.restassured.matcher.RestAssuredMatchers.*;
 
-import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.*;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
+/**
+ * @author gerardo
+ *
+ */
 public class ClientResourceRESTServiceTest {
 	
 
 	@Test
 	public final void testListAllClients() {
         given().when().get("http://localhost:8081/EjbJaxRS-web/rest/clientes/").then()
-        .body("nombre", Matchers.contains(Matchers.equalTo("Jose")));
+        .body("nombre", contains(equalTo("Jose")));
         
         
 	}
